0391855d0e493db75f7ac76bdb87f03f0ef54a14
smj-21 <87064430+smj-21@users.noreply.github.com>
2026-04-12 16:32:25 +0530

    Update Customer_behavior_analysis_code.sql

--- Customer_behavior_analysis_code.sql
+++ Customer_behavior_analysis_code.sql
@@ -80,21 +80,3 @@ FROM customer_types
 GROUP BY customer_segment
 ORDER BY total_count DESC
 
-SELECT category,
-           item_purchased,
-           COUNT(customer_id) AS total_orders,
-           ROW_NUMBER() OVER (PARTITION BY category ORDER BY COUNT(customer_id) DESC) AS item_rank
-    FROM customer_behavior
-    GROUP BY category, item_purchased
-
-WITH item_counts AS (
-    SELECT category,
-           item_purchased,
-           COUNT(customer_id) AS total_orders,
-           ROW_NUMBER() OVER (PARTITION BY category ORDER BY COUNT(customer_id) DESC) AS item_rank
-    FROM customer_behavior
-    GROUP BY category, item_purchased
-)
-SELECT item_rank,category, item_purchased, total_orders
-FROM item_counts
-WHERE item_rank <=3;
